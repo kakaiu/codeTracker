93ba2c66acb602692406a6aca44750eab8c70019
Zhenghui Jin <zjin@vmware.com>
2017-12-04 16:07:41 +0800

    Update

--- ast/AST_Visualization.py
+++ ast/AST_Visualization.py
@@ -6,77 +6,118 @@ __author__  = 'king-jojo'
 from graphviz import Digraph
 from AST_Process import Node_extract
 import re
+import os
 
 RE_AZ = re.compile(r'-(.*?) ')
 RE_C = re.compile(r'/(.*).c')
 
 def node_graph(code_path, preprocess, same_list):
     """Show the syntax tree and display the same part of tow trees"""
-    node_list = Node_extract(code_path, preprocess)
-    num_list = []
-    name = re.findall(RE_C, code_path)
-    for x in range(1, len(node_list)):
-        num = node_list[x]['_nodetype'].find('-')
-        num_list.append(num)
-        cut = re.findall(RE_AZ, node_list[x]['_nodetype'])
-        if len(cut) > 0:
-            node_list[x]['_nodetype'] = cut[0]
-        else:
-            node_list[x]['_nodetype'] = ''
-    num_list.insert(0,0)
-    if same_list == None:
-        num_cp = num_list
-        dot = Digraph(comment = name, format="pdf")
-        dot.node('0', 'TranslationUnitDecl')
-        for y in range(1, max(num_cp)+1, 2):
-            if y == 1:
-                for x in range(len(node_list)):
-                    if num_list[x] == y:
-                        dot.node(str(x), node_list[x]['_nodetype'])
-                        dot.edge('0', str(x))
-            if y > 1:
-                for x in range(len(num_cp)):
-                    if num_list[x] == y:
-                        dot.node(str(x), node_list[x]['_nodetype'])
-                        for z in range(x, 0, -1):
-                            if num_list[z] == y-2:
-                                dot.edge(str(z), str(x))
-                                break
+    if isinstance(code_path, list):
+        dot = Digraph("G", format="pdf")
+        dot.node('Start', 'Start')
+        for i in range(len(code_path)):
+            node_list = Node_extract(code_path[i], preprocess)
+            num_list = []
+            name = re.findall(RE_C, code_path[i])
+            for x in range(1, len(node_list)):
+                num = node_list[x]['_nodetype'].find('-')
+                num_list.append(num)
+                cut = re.findall(RE_AZ, node_list[x]['_nodetype'])
+                if len(cut) > 0:
+                    node_list[x]['_nodetype'] = cut[0]
+                else:
+                    node_list[x]['_nodetype'] = ''
+            num_list.insert(0, 0)
+            if same_list == None:
+                num_cp = num_list
+                sub = Digraph(comment=name)
+                sub.node(str(i)+'a0', str(name))
+                for y in range(1, max(num_cp) + 1, 2):
+                    if y == 1:
+                        for x in range(len(node_list)):
+                            if num_list[x] == y:
+                                sub.node(str(i) + 'b' + str(x), node_list[x]['_nodetype'])
+                                sub.edge(str(i)+'a0', str(i) + 'b' + str(x))
+                    if y > 1:
+                        for x in range(len(num_cp)):
+                            if num_list[x] == y:
+                                sub.node(str(i) + 'b' + str(x), node_list[x]['_nodetype'])
+                                for z in range(x, 0, -1):
+                                    if num_list[z] == y - 2:
+                                        sub.edge(str(i) + 'b' + str(z), str(i) + 'b' + str(x))
+                                        break
+            dot.subgraph(sub)
+            dot.edge('Start', str(i)+'a0')
+        save_name = './output/combine'
+        dot.view(save_name)
+        if os.path.getsize(save_name + '.pdf') > 200000:
+            raise SystemExit("Error: The graph is too large for evince")
     else:
-        num_cp = num_list
-        same_cp = same_list
-        dot = Digraph(comment = name, format="pdf")
-        if 0 in same_cp:
-            dot.node('0', 'TranslationUnitDecl', _attributes={"color":"red"})
-        else:
+        node_list = Node_extract(code_path, preprocess)
+        num_list = []
+        name = re.findall(RE_C, code_path)
+        for x in range(1, len(node_list)):
+            num = node_list[x]['_nodetype'].find('-')
+            num_list.append(num)
+            cut = re.findall(RE_AZ, node_list[x]['_nodetype'])
+            if len(cut) > 0:
+                node_list[x]['_nodetype'] = cut[0]
+            else:
+                node_list[x]['_nodetype'] = ''
+        num_list.insert(0,0)
+        if same_list == None:
+            num_cp = num_list
+            dot = Digraph(comment = name, format="pdf")
             dot.node('0', 'TranslationUnitDecl')
-        for y in range(1, max(num_cp) + 1, 2):
-            if y == 1:
-                for x in range(len(node_list)):
-                    if num_list[x] == y:
-                        if x in same_cp:
-                            dot.node(str(x), node_list[x]['_nodetype'], _attributes={"color":"red"})
-                            dot.edge('0', str(x))
-                        else:
+            for y in range(1, max(num_cp)+1, 2):
+                if y == 1:
+                    for x in range(len(node_list)):
+                        if num_list[x] == y:
                             dot.node(str(x), node_list[x]['_nodetype'])
                             dot.edge('0', str(x))
-            if y > 1:
-                for x in range(len(num_cp)):
-                    if num_list[x] == y:
-                        if x in same_cp:
-                            dot.node(str(x), node_list[x]['_nodetype'], _attributes={"color":"red"})
-                            for z in range(x, 0, -1):
-                                if num_list[z] == y - 2:
-                                    dot.edge(str(z), str(x))
-                                    break
-                        else:
+                if y > 1:
+                    for x in range(len(num_cp)):
+                        if num_list[x] == y:
                             dot.node(str(x), node_list[x]['_nodetype'])
                             for z in range(x, 0, -1):
-                                if num_list[z] == y - 2:
+                                if num_list[z] == y-2:
                                     dot.edge(str(z), str(x))
                                     break
-    save_name = './output/'+str(name[0])
-    dot.view(save_name)
-
-
-
+        else:
+            num_cp = num_list
+            same_cp = same_list
+            dot = Digraph(comment = name, format="pdf")
+            if 0 in same_cp:
+                dot.node('0', 'TranslationUnitDecl', _attributes={"color":"red"})
+            else:
+                dot.node('0', 'TranslationUnitDecl')
+            for y in range(1, max(num_cp) + 1, 2):
+                if y == 1:
+                    for x in range(len(node_list)):
+                        if num_list[x] == y:
+                            if x in same_cp:
+                                dot.node(str(x), node_list[x]['_nodetype'], _attributes={"color":"red"})
+                                dot.edge('0', str(x))
+                            else:
+                                dot.node(str(x), node_list[x]['_nodetype'])
+                                dot.edge('0', str(x))
+                if y > 1:
+                    for x in range(len(num_cp)):
+                        if num_list[x] == y:
+                            if x in same_cp:
+                                dot.node(str(x), node_list[x]['_nodetype'], _attributes={"color":"red"})
+                                for z in range(x, 0, -1):
+                                    if num_list[z] == y - 2:
+                                        dot.edge(str(z), str(x))
+                                        break
+                            else:
+                                dot.node(str(x), node_list[x]['_nodetype'])
+                                for z in range(x, 0, -1):
+                                    if num_list[z] == y - 2:
+                                        dot.edge(str(z), str(x))
+                                        break
+        save_name = './output/'+str(name[0])
+        dot.view(save_name)
+        if os.path.getsize(save_name + '.pdf') > 200000:
+            raise SystemExit("Error: The graph is too large for evince")
--- ast/main.py
+++ ast/main.py
@@ -15,16 +15,23 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 RE_AZ = re.compile(r'-(.*?) ')
 RE_C = re.compile(r'/(.*?).c')
+sel = True
 
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args)>0:
         if args[0] == '--compare':
-            if len(args[1:]) == 2:
+            if len(args[1:]) == 3:
+                if args[3] != 'True' and args[3] != 'False':
+                    raise SystemExit("Please use 'True' or 'False' to choose whether you need to remove the headers")
+                else:
+                    if args[3] == 'True':
+                        sel = True
+                    else:
+                        sel = False
                 if os.path.exists(args[1]) and os.path.exists(args[2]):
                     code_path_a = args[1]
                     code_path_b = args[2]
-                    sel = True
                     seq1 = AST_Compare.seq_process(code_path_a, sel)
                     seq2 = AST_Compare.seq_process(code_path_b, sel)
                     set = AST_Compare.Seqlist_compare(seq1, seq2)
@@ -37,24 +44,37 @@ if __name__ == '__main__':
                 if not os.path.exists(args[1]) and not os.path.exists(args[2]):
                     raise SystemExit("Error: Could not find both two files")
             else:
-                raise SystemExit("Usage: python main.py --compare c_file_dir1 c_file_dir2")
+                raise SystemExit("Usage: python main.py --compare c_file_dir1 c_file_dir2 True/False ")
         elif args[0] == '--view':
-            if len(args[1:]) == 1:
+            if len(args[1:]) == 2:
+                if args[2] != 'True' and args[2] != 'False':
+                    raise SystemExit("Please use 'True' or 'False' to choose whether you need to remove the headers")
+                else:
+                    if args[2] == 'True':
+                        sel = True
+                    else:
+                        sel = False
                 if os.path.exists(args[1]):
                     code_path_a = args[1]
-                    sel = True
                     seq1 = AST_Compare.seq_process(code_path_a, sel)
                     node_graph(code_path_a, sel, None)
                 else:
                     raise SystemExit("Error: Could not find c/c++ file")
             else:
-                raise SystemExit("Usage: python main.py --view c_file_dir")
+                raise SystemExit("Usage: python main.py --view c_file_dir True/False ")
         elif args[0] == '--tojson':
-            if len(args[1:]) == 1:
+            if len(args[1:]) == 2:
+                if args[2] != 'True' and args[2] != 'False':
+                    raise SystemExit("Please use 'True' or 'False' to choose whether you need to remove the headers")
+                else:
+                    if args[2] == 'True':
+                        sel = True
+                    else:
+                        sel = False
                 if os.path.exists(args[1]):
                     code_path_a = args[1]
-                    sel = True
                     node_list = Node_extract(code_path_a, sel)
+                    print ('The total amount of the nodes is {}'.format(len(node_list)))
                     json_files_dir = dir_path + '/jsons'
                     if not os.path.exists(json_files_dir):
                         os.mkdir(json_files_dir)
@@ -64,8 +84,29 @@ if __name__ == '__main__':
                 else:
                     raise SystemExit("Error: Could not find c/c++ file")
             else:
-                raise SystemExit("Usage: python main.py --tojson c_file_dir")
+                raise SystemExit("Usage: python main.py --tojson c_file_dir True/False")
+        elif args[0] == '--combine':
+            if len(args[1:]) == 3:
+                if args[3] != 'True' and args[3] != 'False':
+                    raise SystemExit("Please use 'True' or 'False' to choose whether you need to remove the headers")
+                else:
+                    if args[3] == 'True':
+                        sel = True
+                    else:
+                        sel = False
+                if os.path.exists(args[1]) and os.path.exists(args[2]):
+                    code_path_a = args[1]
+                    code_path_b = args[2]
+                    node_graph([code_path_a, code_path_b], sel, None)
+                if not os.path.exists(args[1]) and os.path.exists(args[2]):
+                    raise SystemExit("Error: Could not find the first c/c++ file")
+                if not os.path.exists(args[2]) and os.path.exists(args[1]):
+                    raise SystemExit("Error: Could not find the second c/c++ file")
+                if not os.path.exists(args[1]) and not os.path.exists(args[2]):
+                    raise SystemExit("Error: Could not find both two files")
+            else:
+                raise SystemExit("Usage: python main.py --combine c_file_dir1 c_file_dir2 True/False ")
         else:
-            raise SystemExit("Usage: python main.py --compare c_file_dir1 c_file_dir2 \n       python main.py --view c_file_dir \n       python main.py --tojson c_file_dir ")
+            raise SystemExit("Usage: python main.py --compare c_file_dir1 c_file_dir2 True/False \n       python main.py --view c_file_dir True/False \n       python main.py --tojson c_file_dir True/False \n       python main.py --combine c_file_dir1 c_file_dir2 True/False  ")
     else:
-        raise SystemExit("Usage: python main.py --compare c_file_dir1 c_file_dir2 \n       python main.py --view c_file_dir \n       python main.py --tojson c_file_dir ")
+        raise SystemExit("Usage: python main.py --compare c_file_dir1 c_file_dir2 True/False \n       python main.py --view c_file_dir True/False \n       python main.py --tojson c_file_dir True/False \n       python main.py --combine c_file_dir1 c_file_dir2 True/False  ")
